2b1557216d802d50ec19f07d27174ca6c0ac3b2a
Andrey V <Andreyxvv@hotmail.com>
2023-02-18 10:09:30 +0500

    first commit

---
+++ HomeWorkCPP2.7.2.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+#define SUB(num1, num2) (num1 - (num2)) \
+
+int main()
+{
+    int a = 6, b = 5, c = 2;
+    std::cout << SUB(a, b) << std::endl;
+    std::cout << SUB(a, b) * c << std::endl;
+    std::cout << SUB(a, b + c) * c << std::endl;
+}
